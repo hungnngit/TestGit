eee2bac3b12eafd7cd4215c54cece5d299f3d7c3
hungngocnguyendt2 <hung.ngocnguyendt2@gmail.com>
2016-06-12 10:50:38 +0700

    change all folder

--- Alarm1/src/com/example/alarm/MainActivity.java
+++ Alarm1/src/com/example/alarm/MainActivity.java
@@ -4,8 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-/**author: internet
-*/
+
 
 public class MainActivity extends Activity {
     SampleAlarmReceiver alarm = new SampleAlarmReceiver();
